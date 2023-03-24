194828df7ce433f9c73beaed33e7ca0fa8712710
Vlada Alekseeva <112939992+alekseevavlada@users.noreply.github.com>
2023-03-24 21:07:21 +0300

    import numpy as np import random import pandas as pd from time import perf_counter import matplotlib.pyplot as plt
    def start():
        array_1 = []
        for i in range(1000000):
            array_1.append(random.random())
        array_2 = []
        for i in range(1000000):
            array_2.append(random.random())
    
        start_time = perf_counter()
    
        time_now = perf_counter() - start_time
        print(time_now)
    
        time_now = perf_counter()
        array_1 = np.random.rand(1000000)
        array_2 = np.random.rand(1000000)
        end = np.multiply(array_1, array_2)
        print('Итог:', perf_counter() - time_now)
    
    x_1 = 'Положение дроссельной заслонки (%)'
    y_2 = 'Обороты двигателя (об/мин)'
    
    def graph():
    
        time = []
        dross = []
        o = []
        with open('data1.csv', 'r') as f:
            for row in f:
                time.append(row['Время'])
                o.append(float(row[x_1]))
                dross.append(float(row[y_2]))
            f.close()
    
        time = np.array(time, float)
        dross = np.array(dross, float)
        o = np.array(o, float)
    
        fig = plt.figure()
        ax = fig.add_subplot(111)
        ax.set_xlim([-10, 10])
        ax.set_ylim([-2, 2])
        ax.set_title('Мой график matplotlib')
        ax.set_xlabel('Время')
        ax.set_ylabel('Положение дроссельной заслонки (%);Обороты двигателя (об/мин)')
        plt.plot(np.array(time), np.array(dross))
        plt.plot(np.array(time), np.array(o))
        plt.show()
    
        plt.title('График корреляции')
        plt.xlabel('Положение дроссельной заслонки (%)')
        plt.ylabel('Обороты двигателя (об/мин)')
        plt.plot(dross, o, 'o')
        plt.show()
    
    def plot3d():
        np.random.seed(40)
        xs = np.linspace(-1*np.pi, 1*np.pi, 100)
        ys = xs
        zs = np.tg(xs)
    
        fig = plt.figure()
        ax = fig.add_subplot(111, projection='3d')
        ax.plot(xs, ys, zs, marker='*', c='pink')
        plt.title('3D  график')
        plt.show()

--- main.py
+++ main.py
@@ -1,55 +1,85 @@
+# Алексеева Влада 367801
 import numpy as np
+import random
+import pandas as pd
+from time import perf_counter
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+
+# Task 1
 
 
 def start():
-    arr = np.array([5, 9, 10, 'ad', 7])
-    arr = arr[::-1]
-    print(arr)
+    array_1 = []
+    for i in range(1000000):
+        array_1.append(random.random())
+    array_2 = []
+    for i in range(1000000):
+        array_2.append(random.random())
+
+    start_time = perf_counter()
+
+    time_now = perf_counter() - start_time
+    print(time_now)
+
+    time_now = perf_counter()
+    array_1 = np.random.rand(1000000)
+    array_2 = np.random.rand(1000000)
+    end = np.multiply(array_1, array_2)
+    print('Итог:', perf_counter() - time_now)
 
 
-def tableLoad():
-    arr = np.genfromtxt('voltage.csv', delimiter=',')
-    time = arr[:100,0]
-    time = time[:,np.newaxis]
-    curr = arr[:100,1]
-    curr = curr[:,np.newaxis]
-    volt = arr[:100,2]
-    volt = volt[:,np.newaxis]
+# Task 2
 
-    plt.plot(time, curr * 50, 'b', time, volt, 'r')
+x_1 = 'Положение дроссельной заслонки (%)'
+y_2 = 'Обороты двигателя (об/мин)'
+
+
+def graph():
+
+    time = []
+    dross = []
+    o = []
+    with open('data1.csv', 'r') as f:
+        for row in f:
+            time.append(row['Время'])
+            o.append(float(row[x_1]))
+            dross.append(float(row[y_2]))
+        f.close()
+
+    time = np.array(time, float)
+    dross = np.array(dross, float)
+    o = np.array(o, float)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_xlim([-10, 10])
+    ax.set_ylim([-2, 2])
+    ax.set_title('Мой график matplotlib')
+    ax.set_xlabel('Время')
+    ax.set_ylabel('Положение дроссельной заслонки (%);Обороты двигателя (об/мин)')
+    plt.plot(np.array(time), np.array(dross))
+    plt.plot(np.array(time), np.array(o))
+    plt.show()
+
+    plt.title('График корреляции')
+    plt.xlabel('Положение дроссельной заслонки (%)')
+    plt.ylabel('Обороты двигателя (об/мин)')
+    plt.plot(dross, o, 'o')
     plt.show()
 
 
-def hist():
-    arr = np.genfromtxt('test.csv', delimiter=',')
-    arr = arr[1:]
-    daysInYear = 365.25
-
-    age = np.int_(arr[:,1] / daysInYear)
-
-    fig = plt.figure(figsize=(6, 4))
-    ax = fig.add_subplot()
-    ax.hist(age, 100, (50, 60))
-    ax.grid()
-    plt.show()
+# Task 3
 
 
 def plot3d():
     np.random.seed(40)
-    xs = np.linspace(0, 10, 20)
+    xs = np.linspace(-1*np.pi, 1*np.pi, 100)
     ys = xs
-    zs = np.sin(xs)
+    zs = np.tg(xs)
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.plot(xs, ys, zs, marker='x', c='red')
+    ax.plot(xs, ys, zs, marker='*', c='pink')
+    plt.title('3D  график')
     plt.show()
 
-
-if __name__ == '__main__':
-    #start()
-    #tableLoad()
-    #hist()
-    plot3d()
