6d37bc0f8a668a82d9abb15baae5ab4fc877060e
Vlada Alekseeva <112939992+alekseevavlada@users.noreply.github.com>
2023-03-27 20:21:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # Алексеева Влада 367801
 import numpy as np
 import random
-import pandas as pd
 from time import perf_counter
+import pandas as pd
 import matplotlib.pyplot as plt
 
 # Task 1
+
 
 def start():
     array_1 = []
@@ -26,37 +27,26 @@ def start():
     end = np.multiply(array_1, array_2)
     print('Итог:', perf_counter() - time_now)
 
+
 # Task 2
 
-x_1 = 'Положение дроссельной заслонки (%)'
-y_2 = 'Обороты двигателя (об/мин)'
+file = pd.read_csv('data1.csv', encoding='cp1251', delimiter=';')
+time = file['Время']
+dross = file['Положение дроссельной заслонки (%)']
+o = file['Обороты двигателя (об/мин)']
 
 
 def graph():
 
-    time = []
-    dross = []
-    o = []
-    with open('data1.csv', 'r') as f:
-        for row in f:
-            time.append(row['Время'])
-            o.append(float(row[x_1]))
-            dross.append(float(row[y_2]))
-        f.close()
-
-    time = np.array(time, float)
-    dross = np.array(dross, float)
-    o = np.array(o, float)
-
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.set_xlim([-10, 10])
-    ax.set_ylim([-2, 2])
+    ax.set_xlim([-10, 50])
+    ax.set_ylim([-2, 1000])
     ax.set_title('Мой график matplotlib')
     ax.set_xlabel('Время')
     ax.set_ylabel('Положение дроссельной заслонки (%);Обороты двигателя (об/мин)')
-    plt.plot(np.array(time), np.array(dross))
-    plt.plot(np.array(time), np.array(o))
+    plt.plot(time, dross)
+    plt.plot(time, o)
     plt.show()
 
     plt.title('График корреляции')
@@ -65,13 +55,15 @@ def graph():
     plt.plot(dross, o, 'o')
     plt.show()
 
+
 # Task 3
+
 
 def plot3d():
     np.random.seed(40)
     xs = np.linspace(-1*np.pi, 1*np.pi, 100)
     ys = xs
-    zs = np.tg(xs)
+    zs = np.tan(xs)
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
@@ -79,3 +71,6 @@ def plot3d():
     plt.title('3D  график')
     plt.show()
 
+start()
+graph()
+plot3d()
